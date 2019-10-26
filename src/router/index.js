b1ffa111273347b2510faf68008f3cd4fa6967b0
mrtkj <45763653+mrtkj@users.noreply.github.com>
2019-10-26 18:49:15 +0900

    add header and footer

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,39 @@
+/* eslint no-unused-vars: 0 */
 import Vue from 'vue'
-// eslint-disable-next-line
 import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from '@/components/home'
+// 追加モジュール
+// import test1 from '@/components/test1'
+// import test2 from '@/components/test2'
+// import test3 from '@/components/test3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(Router)
+Vue.use(BootstrapVue)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: home
     }
+    // {
+    //   path: '/test1',
+    //   name: 'test1',
+    //   component: test1
+    // },
+    // {
+    //   path: '/test2',
+    //   name: 'test2',
+    //   component: test2
+    // },
+    // {
+    //   path: '/test3',
+    //   name: 'test3',
+    //   component: test3
+    // }
   ]
 })
