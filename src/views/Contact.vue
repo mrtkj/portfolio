ee317546196235a69ea3ef6edd3b05a89d0963e3
mrtkj <45763653+mrtkj@users.noreply.github.com>
2019-11-07 13:27:41 +0900

    edit contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,11 +1,38 @@
 <template>
   <div class="contact">
     <div class="container pt-5 pb-5">
-      <div class="row mb-2  d-flex justify-content-center">
+      <div class="row mb-5  d-flex justify-content-center">
         <p class="border-bottom inline-block text-nowrap h2">
           Contact
         </p>
       </div>
+      <div class="row justify-content-center">
+        <div class="card col-md-4 col-12">
+          <div class="card-title mt-2 mb-0 border-bottom">email</div>
+          <div class="card-body">
+            <button v-on:click="sendMail">
+              <span
+                class="iconify"
+                data-icon="simple-line-icons:envelope"
+                data-inline="false"
+              ></span>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Contact",
+  methods: {
+    sendMail: function(event) {
+      if (event) {
+        alert("test");
+      }
+    }
+  }
+};
+</script>
